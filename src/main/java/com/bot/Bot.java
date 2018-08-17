097ec86d26f6adb373e70636d917e9aeced088a1
Roman Budnyk <r.m.budnik@gmail.com>
2018-08-17 15:19:53 +0300

    register bot in main method

--- src/main/java/com/bot/Bot.java
+++ src/main/java/com/bot/Bot.java
@@ -1,4 +1,25 @@
 package com.bot;
 
-public class Bot {
+import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.meta.api.objects.Update;
+
+import java.util.List;
+
+public class Bot extends TelegramLongPollingBot {
+
+    public void onUpdateReceived(Update update) {
+
+    }
+
+    public void onUpdatesReceived(List<Update> updates) {
+
+    }
+
+    public String getBotUsername() {
+        return null;
+    }
+
+    public String getBotToken() {
+        return null;
+    }
 }
